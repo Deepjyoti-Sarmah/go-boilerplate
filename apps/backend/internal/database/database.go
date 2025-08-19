b9dc5768a42be97a19721cf6220d997fdd949d4d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-08-19 16:20:07 +0530

    adding db tracer for pgx

--- apps/backend/internal/database/database.go
+++ apps/backend/internal/database/database.go
@@ -2,8 +2,10 @@
 package database
 
 import (
+	"context"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 )
@@ -11,6 +13,34 @@ import (
 type Database struct {
 	Pool *pgxpool.Pool
 	log  *zerolog.Logger
+}
+
+// multiTracer allows chaining mulitple tracer
+type multiTracer struct {
+	tracers []any
+}
+
+// TraceQueryStart implements pgx tracer interface
+func (mt *multiTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryStart(context.Context, *pgx.Conn, pgx.TraceQueryStartData) context.Context
+		}); ok {
+			ctx = t.TraceQueryStart(ctx, conn, data)
+		}
+	}
+	return ctx
+}
+
+// TraceQueryEnd implements pgx tracer interface
+func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryEnd(context.Context, *pgx.Conn, pgx.TraceQueryEndData)
+		}); ok {
+			t.TraceQueryEnd(ctx, conn, data)
+		}
+	}
 }
 
 const DatabasePingTimeout = 10 * time.Second
